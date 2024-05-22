8e18b219ea24207a6cb3d71a16927af8bef10926
imbirovsky <sergey.imbirovskiy@gmail.com>
2024-05-22 19:01:53 +0300

    TEST#10099

--- chart/send_telegram.py
+++ chart/send_telegram.py
@@ -52,7 +52,7 @@ def format_test_message(status, count, get_tests_func, allure_report_path):
             if len(name_parts) > 1 and is_url(name_parts[1]):
                 message += f'\t\t<a href="{name_parts[1]}">{name_parts[1]}</a>\n'
             if test['response_code']:
-                message += f"\t\t<code>{test['response_code']}</code>\n"
+                message += f"\t\t<code>{test['response_code']}</code>"
             message += "\n"
         if len(tests) > 5:
             message += f"And {len(tests) - 5} more {status} tests...\n"
