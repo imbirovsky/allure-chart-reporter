6f5a84864bc7336dc41fd0a72d2c714d84ecbac0
imbirovsky <sergey.imbirovskiy@gmail.com>
2024-05-22 20:41:37 +0300

    000000009000

--- chart/send_telegram.py
+++ chart/send_telegram.py
@@ -66,7 +66,7 @@ def send_photo_and_message(token, chat_id, photo_path, total, passed, failed, br
     message += "\n"  # Add a newline before the footer
     message += centered_header  # Add the centered footer
     message += "\n"  # Add a newline after the footer
-    message = f"<b>Tests completed<b>\n\n"
+    message = f"<b>Tests completed</b>\n\n"
     message += f"<b>• Total: {total}</b>\n\n"
     if int(passed) > 0:
         message += f"<b>• Passed: {passed}</b>\n"
