032e6eaba180f4df94ae684908d006d2c31692b7
imbirovsky <sergey.imbirovskiy@gmail.com>
2024-05-22 19:32:47 +0300

    T1234567890

--- chart/send_telegram.py
+++ chart/send_telegram.py
@@ -61,11 +61,14 @@ def format_test_message(status, count, get_tests_func, allure_report_path):
 def send_photo_and_message(token, chat_id, photo_path, total, passed, failed, broken, skipped, report_link, allure_report_path):
     url = f"https://api.telegram.org/bot{token}/sendPhoto"
     message = "Tests completed\n\n"
-    message += f"<b>• Total: {total}</b>\n"
+    message += f"<b>• Total: {total}</b>\n\n"
     if int(passed) > 0:
         message += f"<b>• Passed: {passed}</b>\n"
+    message += "\n"
     message += format_test_message('failed', failed, get_failed_tests, allure_report_path)
+    message += "\n"
     message += format_test_message('broken', broken, get_broken_tests, allure_report_path)
+    message += "\n"
     message += format_test_message('skipped', skipped, get_skipped_tests, allure_report_path)
 
     # Check if the message exceeds the limit
